dc998a601294c01736c387f106725f16274292aa
Doug Branton <dougrbranton@gmail.com>
2015-10-15 11:48:06 -0700

    Finished skeleton structure of Nightlog

--- tccv3.py
+++ tccv3.py
@@ -681,12 +681,34 @@ class NightLog(wx.ScrolledWindow):
         
         #Failure Log
         self.failheader=wx.StaticText(self,label="FAILURE LOG")
+        self.failinfo=wx.StaticText(self,label="Time                                                                          Description                                                                                    ")
         self.faillog=wx.TextCtrl(self, size=(600,50),style= wx.TE_MULTILINE)
         
         #Focus Log
         self.focheader=wx.StaticText(self,label="FOCUS LOG")
-        self.focinfo=wx.StaticText(self,label='Time            Instrument            Focus              Az      El      Temp    Strc    Prim     Sec     Air     filt     FWHM')
+        self.focinfo=wx.StaticText(self,label='Time               Instrument               Focus                 Az      El      Temp    Strc    Prim     Sec     Air     filt     FWHM')
         self.foclog=wx.TextCtrl(self, size=(600,100),style= wx.TE_MULTILINE)
+        
+        self.focusButton=wx.Button(self,label="Number of Focus Lines")
+       # self.Bind(wx.EVT_BUTTON,self.getFocus,self.focusButton)
+        self.focusNum=wx.TextCtrl(self,size=(30,-1))
+        self.focusNum.AppendText('1')
+        
+        self.hbox6=wx.BoxSizer(wx.HORIZONTAL)
+        self.hbox6.Add(self.focusButton,0,wx.ALIGN_CENTER)
+        self.hbox6.AddSpacer(5)
+        self.hbox6.Add(self.focusNum,0,wx.ALIGN_CENTER)
+        
+        #Save as and Submit buttons
+        self.saveButton=wx.Button(self,label="Save As")
+        
+        self.submitButton=wx.Button(self,label="Submit")
+        
+        self.hbox7=wx.BoxSizer(wx.HORIZONTAL)
+        self.hbox7.Add(self.saveButton,0,wx.ALIGN_CENTER)
+        self.hbox7.AddSpacer(25)
+        self.hbox7.Add(self.submitButton,0,wx.ALIGN_CENTER)
+        
         
         #Vertical box organization
         self.vbox=wx.BoxSizer(wx.VERTICAL)
@@ -705,11 +727,21 @@ class NightLog(wx.ScrolledWindow):
         self.vbox.AddSpacer(10)
         self.vbox.Add(self.actheader,0,wx.ALIGN_CENTER)
         self.vbox.Add(self.actlog,0,wx.ALIGN_CENTER)
+        self.vbox.AddSpacer(5)
         self.vbox.Add(self.failheader, 0, wx.ALIGN_CENTER)
+        self.vbox.AddSpacer(5)
+        self.vbox.Add(self.failinfo,0,wx.ALIGN_CENTER)
         self.vbox.Add(self.faillog,0, wx.ALIGN_CENTER)
+        self.vbox.AddSpacer(5)
         self.vbox.Add(self.focheader,0,wx.ALIGN_CENTER)
+        self.vbox.AddSpacer(5)
         self.vbox.Add(self.focinfo,0,wx.ALIGN_CENTER)
         self.vbox.Add(self.foclog,0,wx.ALIGN_CENTER)
+        self.vbox.AddSpacer(5)
+        self.vbox.Add(self.hbox6,0,wx.ALIGN_CENTER)
+        self.vbox.AddSpacer(25)
+        self.vbox.Add(self.hbox7,0,wx.ALIGN_CENTER)
+        
         
         self.SetSizer(self.vbox)
         self.Show()
@@ -968,6 +1000,12 @@ class TCC(wx.Frame):
     def test(self,event):
         print 'this is a test event'
         return
+        
+    def getFocus(self,event):
+        num=self.focusNum.GetValue()
+        files=os.popen('tail -%s /Users/%s/nfocus.txt' % (num, os.getenv('USER')), 'r')
+        for l in files:
+            self.focusLog.AppendText(l)
 
 if __name__=="__main__":
   app = wx.App()
