5250f478037b6ad815030623e5b110ce45f2ff26
Doug <dougrbranton@gmail.com>
2015-12-05 12:37:12 -0800

    Initial interfacing layers

---
+++ TCC_parser.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python
+
+#import pmc
+
+class Parser(object):
+	def __init__(self):
+		self.pmc = pmc.pmc()
+
+	def parse(self, input = None):
+		print input
+		input = input.split()
+		if input[0] == 'slew':
+			coords_dict={'ra':input[1],
+				'dec':input[2]}
+			logread= "Slewing telescope"
+			return {'log':logread, 'slew':	self.pmc.slew(coords_dict)}
+
+		if input[0]=='focus':
+			value=input[1]
+			logread= "Focusing telescope"
+			return {'log':logread, 'focus': self.pmc.slew(value)
+
+		if input[0]=='offset':
+			RAjog=input[1]
+			DECjog=input[2]
+			logread="Jogging telescope by offset parameters"
+			return {'log':logread, 'offset':self.pmc.offset(RAjog, DECjog)}
+
+		if input[0]=='toggle track':
+			tracking=input[1]
+			if tracking=True:
+				logread= "Turning tracking on"
+			elif tracking=False:
+				logread= "Turning tracking off"
+			return {'log':logread,'track_toggle':self.pmc.track_set(tracking)
+
+		if input[0]=='set tracking rate':
+			RArate=input[1]
+			DECrate=input[2]
+			logread:"Setting RA and DEC tracking rates to specified values"
+			return {'log':logread1,  'trackingrate':self.pmc.track_setrate(RArate,DECrate)
+
+		if input[0]=='tracking status':
+			return self.pmc.track_status()
+			
+
--- pmc.py
+++ pmc.py
@@ -10,6 +10,11 @@ class pmc():
 		self.conf = self._read_conf(conf_fn)
 
 	def slew(self, coords_dict):
+		'''
+		takes in a RA, DEC coordinates to prompt hardware to 			initiate a target slew
+		arguments: coords_dict
+		returns: slewing confirmation
+		'''
 		coords = SkyCoord(ra=coords_dict['ra'],
 				dec=coords_dict['dec'],
 				frame='icrs',
@@ -21,16 +26,55 @@ class pmc():
 		# call bct30 slew
 
 	def track(self):
+		'''
+		track_onoroff
+		track_setrate
+		track_status
+		'''
 		None
+		#DB Does this mean tracking on or off?	
+
 		# check soft limits in conf to stop tracking when alt < conf limit
 		# find polar axis soft limit, RA, limit on non-setting stars
 		# find declination wrap limit (RA +/- 10hrs)
 		# call bct30 track
+	
+	def track_set(self,tracking=False):
+		'''
+		Sets tracking to be on or off
+		arguments: tracking (Boolean)
+		returns: confirmation of tracking preference
+		'''
+		None
+	def track_setrate(self, RArate, DECrate):
+		'''
+		sets the individual tracking rates for RA and DEC
+		arguments: RArate, DECrate
+		returns: confirmation and set rates
+		'''
+		None
+	def track_status(self)
+		'''
+		Displays status information for guider tracking
+		arguments:
+		returns:tracking status
+		'''
+		None 
 
-	def focus(self):
+	def focus(self,value=None):
+		'''
+		this takes in a focus value to focus the telescope
+		arguments: value
+		returns: focus completion status
+		'''
 		None
 
-	def offset(self):
+	def offset(self,RAjog=None,DECjog=None):
+		'''
+		alters the coordinates by an RA and DEC offset
+		arguments: RAjog, DECjog
+		returns: offset amount and confirmation
+		'''
 		# use Jog
 		None
 
