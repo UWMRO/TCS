f3513df551df7e82b6d8205c170210a1a4b49f16
dougbrn <dougrbranton@gmail.com>
2017-05-26 19:18:59 -0700

    Separator

--- parsercode/TCCserver.cpp
+++ parsercode/TCCserver.cpp
@@ -528,7 +528,7 @@ void Listener(void) {
 
 	std::string input = (std::string) incoming_data_buffer;
 	std::vector <std::string> commands = separator(input);
-	for(int i=0; i <commands.size(); i++)
+	for(int i=0; i <commands.size(); i++) {
 		std::cout << commands[i] << std::endl;
 		const char* results = parser(commands[i]);
 		std::cout << results << std::endl;
@@ -539,7 +539,7 @@ void Listener(void) {
 				}
 			}
 		}
-
+	}
 
   }
   // clean up
